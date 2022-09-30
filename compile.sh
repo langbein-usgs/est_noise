b26cf1ca43b7f0897679825891b7fd5e9cf50c15
John Langbein <langbein@usgs.gov>
2022-09-30 08:38:11 -0700

    Add files via upload

--- compile.sh
+++ compile.sh
@@ -14,6 +14,16 @@ then
   exit
 fi
 
+######
+#  for intel compiler and MKL library, see
+#      https://www.intel.com/content/www/us/en/developer/articles/tool/oneapi-standalone-components.html#fortran
+#   get two downloads --- one for the compiler and a second for mkl
+#   get the "off-line" versions
+#   Installation is easy (use sudo on linux)
+#   To setup compiler options, use
+#     https://www.intel.com/content/www/us/en/developer/tools/oneapi/onemkl-link-line-advisor.html#gs.dq7k7t
+##########
+
 ver=7.30
 
 file=`echo "-o bin/est_noise"$ver" ./src/est_noise"$ver".f ./src/time.f  ./src/funmin"$ver".f ./src/dfft.f ./src/NedlerMeadSimplex"$ver".f  ./src/modify.f ./src/NedlerMeadSimplexEXP"$ver".f" ./src/filterfunc.f ./src/genNoise.f src/randnor.f ./src/GetData.f  `
@@ -30,7 +40,7 @@ filebust=`echo -o ./bin/bust_5 ./src/bust_5.f ./src/time.f ./src/GetData.f `
 filewand=`echo -o bin/compare_wander7.01 src/compare_wander7.01.f src/time.f src/GetData.f src/filterfunc.f src/dfft.f src/randnor.f `
 
 fileadj=`echo -o bin/adjust_1 src/adjust_1.f src/time.f src/GetData.f `
-rm -f bin/est_noise"$ver" bin/psd_calc7 bin/gen_noise7.02 bin/adjust* bin/compare_wander*
+rm -f bin/est_noise"$ver" bin/psd_calc7 bin/gen_noise7.02 bin/adjust* bin/compare_wander* bin/bust*
 
 if [ "$1" = "linux" ]
 then
@@ -62,21 +72,41 @@ then
   then
     echo compiler is intel ifort
      echo compile est_noise7
-    MKLPATH=/opt/intel/mkl/lib/intel64
-    MKLINCLUDE=/opt/intel/mkl/include/intel64
-    ifort $file -mcmodel medium -shared-intel   -warn none -heap-arrays 2048  -L$MKLPATH -I$MKLINCLUDE -I$MKLINCLUDE/lp64 -lmkl_lapack95_lp64 \
-  -Wl,--start-group $MKLPATH/libmkl_intel_lp64.a $MKLPATH/libmkl_intel_thread.a $MKLPATH/libmkl_core.a \
-   /opt/intel/lib/intel64/libiomp5.a   -lpthread -Wl,--end-group
+#  does static linking using free version of intel fortran and mkl;  version 2021.6.0
+     source /opt/intel/oneapi/setvars.sh --force
+     ifort $file -I${MKLROOT}/include/intel64/ilp64 -i8  -I"${MKLROOT}/include" \
+      ${MKLROOT}/lib/intel64/libmkl_blas95_ilp64.a ${MKLROOT}/lib/intel64/libmkl_lapack95_ilp64.a \
+      -Wl,--start-group ${MKLROOT}/lib/intel64/libmkl_intel_ilp64.a ${MKLROOT}/lib/intel64/libmkl_intel_thread.a \
+     ${MKLROOT}/lib/intel64/libmkl_core.a -Wl,--end-group \
+      -liomp5 -lpthread -lm -ldl \
+      -mcmodel medium -shared-intel   -warn none -heap-arrays 2048 -O3
      echo compile psd_calc7
-    ifort $filepsd -O3 -mcmodel medium 
+    ifort $filepsd -O3 -mcmodel medium \
+      -i8  -I"${MKLROOT}/include" \
+        -Wl,--start-group ${MKLROOT}/lib/intel64/libmkl_intel_ilp64.a ${MKLROOT}/lib/intel64/libmkl_intel_thread.a \
+         ${MKLROOT}/lib/intel64/libmkl_core.a -Wl,--end-group -liomp5 -lpthread -lm -ldl
      echo compile gen_noise
-    ifort $filegen -O3 -mcmodel medium 
+    ifort $filegen -O3 -mcmodel medium\
+      -i8  -I"${MKLROOT}/include" \
+        -Wl,--start-group ${MKLROOT}/lib/intel64/libmkl_intel_ilp64.a ${MKLROOT}/lib/intel64/libmkl_intel_thread.a \
+         ${MKLROOT}/lib/intel64/libmkl_core.a -Wl,--end-group -liomp5 -lpthread -lm -ldl
      echo compile compare_wander
-    ifort $filewand -O3 -mcmodel medium 
+    ifort $filewand -O3 -mcmodel medium \
+      -i8  -I"${MKLROOT}/include" \
+        -Wl,--start-group ${MKLROOT}/lib/intel64/libmkl_intel_ilp64.a ${MKLROOT}/lib/intel64/libmkl_intel_thread.a \
+         ${MKLROOT}/lib/intel64/libmkl_core.a -Wl,--end-group -liomp5 -lpthread -lm -ldl
      echo compile bust_5
-    ifort $filebust -O3
+    ifort $filebust -O3 \
+      -i8  -I"${MKLROOT}/include" \
+        -Wl,--start-group ${MKLROOT}/lib/intel64/libmkl_intel_ilp64.a ${MKLROOT}/lib/intel64/libmkl_intel_thread.a \
+         ${MKLROOT}/lib/intel64/libmkl_core.a -Wl,--end-group -liomp5 -lpthread -lm -ldl
      echo compile adjust
-    ifort $fileadj -O3
+    ifort $fileadj -O3 \
+      -i8  -I"${MKLROOT}/include" \
+        -Wl,--start-group ${MKLROOT}/lib/intel64/libmkl_intel_ilp64.a ${MKLROOT}/lib/intel64/libmkl_intel_thread.a \
+         ${MKLROOT}/lib/intel64/libmkl_core.a -Wl,--end-group -liomp5 -lpthread -lm -ldl
+
+
   else
     echo "Invalid compiler option"
     exit
@@ -93,7 +123,9 @@ then
 #  currently works 
 #     gfortran $file -O3 -framework Accelerate -mcmodel=medium  -Wa,-q -w -std=legacy
 #  use with openblas from homebrew
-     gfortran $file -I/usr/include/openblas/ -I/usr/local/Cellar/openblas/0.3.21/include/  -lopenblas -mcmodel=medium -O3 -w -std=legacy  -L/usr//local/Cellar/openblas/0.3.21/lib/  
+     gfortran $file -I/usr/include/openblas/ -I/usr/local/Cellar/openblas/0.3.21/include/ \
+       -lopenblas -mcmodel=medium -O3 -w -std=legacy  -L/usr//local/Cellar/openblas/0.3.21/lib/   \
+       -lpthread -lm -ldl -lgomp
 #  use with valgrind
 #     gfortran $file -O0 -g -framework Accelerate  -mcmodel=medium -Wl,-stack_size,0x80000000   #  -Wa,-q
 #     exit
@@ -112,26 +144,58 @@ then
   then
     echo compiler is intel ifort
     echo compile est_noise
-    /opt/intel/bin/compilervars.sh intel64
-    /opt/intel/mkl/bin/mklvars.sh intel64
+#  used for older version of ifort
+#    /opt/intel/bin/compilervars.sh intel64
+#    /opt/intel/mkl/bin/mklvars.sh intel64
  
-    ifort $file -w -i8 -I${MKLROOT}/include/intel64/ilp64 -I${MKLROOT}/include -O3 \
-    ${MKLROOT}/lib/libmkl_blas95_ilp64.a ${MKLROOT}/lib/libmkl_intel_ilp64.a ${MKLROOT}/lib/libmkl_sequential.a ${MKLROOT}/lib/libmkl_core.a -lpthread -lm -ldl \
-         -Wl,-stack_size,0x40000000 
+#   ifort $file -w -i8 -I${MKLROOT}/include/intel64/ilp64 -I${MKLROOT}/include -O3 \
+#    ${MKLROOT}/lib/libmkl_blas95_ilp64.a ${MKLROOT}/lib/libmkl_intel_ilp64.a ${MKLROOT}/lib/libmkl_sequential.a ${MKLROOT}/lib/libmkl_core.a -lpthread -lm -ldl \
+#         -Wl,-stack_size,0x40000000 
+
+
+     
+     source /opt/intel/oneapi/setvars.sh --force
+     echo MKLROOT $MKLROOT
+     echo INCLUDE $INCLUDE
+     echo LD_LIB $LD_LIBRARY_PATH
+     echo LIB $LIBRARY_PATH
+     
+
+#  dynamic linking     
+#     ifort $file -Ofast -I${MKLROOT}/include/intel64/ilp64 -i8  -I"${MKLROOT}/include" \
+#     ${MKLROOT}/lib/libmkl_blas95_ilp64.a ${MKLROOT}/lib/libmkl_lapack95_ilp64.a \
+#     -L${MKLROOT}/lib -Wl,-rpath,${MKLROOT}/lib -lmkl_intel_ilp64 -lmkl_intel_thread \
+#     -lmkl_core -liomp5 -lpthread -lm -ldl \
+#     -L/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib \
+#     -Wl,-stack_size,0x40000000 -static-intel
+
+#  static linking?
+    ifort $file -Ofast -I${MKLROOT}/include/intel64/ilp64 -i8  -I"${MKLROOT}/include" \
+    ${MKLROOT}/lib/libmkl_blas95_ilp64.a ${MKLROOT}/lib/libmkl_lapack95_ilp64.a \
+    ${MKLROOT}/lib/libmkl_intel_ilp64.a ${MKLROOT}/lib/libmkl_intel_thread.a \
+    ${MKLROOT}/lib/libmkl_core.a -liomp5 -lpthread -lm -ldl \
+    -L/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib \
+     -Wl,-stack_size,0x40000000 -static-intel 
+#     ifort $file -Ofast -I${MKLROOT}/include/intel64/ilp64 -i8  -I"${MKLROOT}/include" \     
+#     ${MKLROOT}/lib/libmkl_blas95_ilp64.a ${MKLROOT}/lib/libmkl_lapack95_ilp64.a \
+#     ${MKLROOT}/lib/libmkl_intel_ilp64.a ${MKLROOT}/lib/libmkl_intel_thread.a \
+#     ${MKLROOT}/lib/libmkl_core.a -liomp5 -lpthread -lm -ldl \    
+#    -L/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib \
+#     -Wl,-stack_size,0x40000000
 
 #    ifort $file -i8 -I${MKLROOT}/include/intel64/ilp64 -I${MKLROOT}/include  ${MKLROOT}/lib/libmkl_blas95_ilp64.a ${MKLROOT}/lib/libmkl_lapack95_ilp64.a -L${MKLROOT}/lib -Wl,-rpath,${MKLROOT}/lib -lmkl_intel_ilp64 -lmkl_sequential -lmkl_core -lpthread -lm -ldl
 #    ifort $file  -Wl,-stack_size,0x40000000 -O3 -i8 -I${MKLROOT}/include/intel64/ilp64 -I${MKLROOT}/include  ${MKLROOT}/lib/libmkl_blas95_ilp64.a ${MKLROOT}/lib/libmkl_lapack95_ilp64.a -L${MKLROOT}/lib -Wl,-rpath,${MKLROOT}/lib -lmkl_intel_ilp64 -lmkl_intel_thread -lmkl_core -liomp5 -lpthread -lm -ldl
 #    ifort $file -Wl,-stack_size,0x40000000 -O3  -L/Library/Frameworks/Intel_MKL.framework/Versions/Current/lib/universal -lmkl_lapack -lmkl_core -lmkl_intel_lp64 -lmkl_intel_ilp64 -lmkl_intel_thread
     echo compile psd_calc7
-    ifort -O3 $filepsd
+    ifort -O3 $filepsd -L/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib 
     echo compile gen_noise
-    ifort -O3 $filegen
+    ifort -O3 $filegen -L/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib
      echo compile compare_wander
-    ifort $filewand -O3
+    ifort $filewand -O3 -L/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib
      echo compile bust_5
-    ifort $filebust -O3
+    ifort $filebust -O3 -L/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib
      echo compile adjust
-    ifort $fileadj -O3
+    ifort $fileadj -O3 -L/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib
   else
     echo "Invalid compiler option"
     exit
@@ -150,3 +214,5 @@ ls -l bin/compare_wander7.01
 ls -l bin/bust_5
 ls -l bin/adjust_1
 exit
+
+
